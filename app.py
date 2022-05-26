83e7ac7509862b7c09fbf548bd5be6b29739ee10
Handis <handis1201@gmail.com>
2022-05-27 00:09:59 +0200

    added sites and scheme

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
-from backend.index import index
+from flask import Flask
+from flask_navigation import Navigation
+
+from backend.index import index_blueprint
+from backend.test import test_blueprint
 
 app = Flask(__name__)
 app.static_folder = "static"
-app.register_blueprint(index, url_prefix='/index')
+app.register_blueprint(index_blueprint, url_prefix='/index')
+app.register_blueprint(test_blueprint, url_prefix='/test')
 
+nav = Navigation(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+nav.Bar('top', [
+    nav.Item('Home', 'index.show'),
+    nav.Item('Test', 'test.show'),
+])
 
 if __name__ == "__main__":
     app.run()
+
+#base.html inside <li...>
+#class="{{ 'active' if item.is_active else '' }}"
--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-index_blueprint = Blueprint('index', __name__, template_folder='templates')
+test_blueprint = Blueprint('test', __name__, template_folder='templates')
 
 
-@index_blueprint.route('/')
+@test_blueprint.route('/')
 def show():
     try:
-        return render_template('index.html')
+        return render_template('test.html')
     except TemplateNotFound:
         abort(404)
