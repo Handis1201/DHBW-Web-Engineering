153e698760933581e0c3345e0a7696774d60909b
Handis <handis1201@gmail.com>
2022-06-04 12:33:39 +0200

    spotify

--- static/javascript/Spotify.js
+++ static/javascript/Spotify.js
@@ -8,11 +8,15 @@ function onButtonClick() {
         let table = document.getElementById("spotify_result");
         table.style.visibility = "visible";
 
+        table.innerHTML = '';
+
         text.items.forEach(item => {
             let row = document.createElement("tr");
             let tdTitleElement = document.createElement("td");
             tdTitleElement.innerHTML = item.name;
             row.appendChild(tdTitleElement);
+
+
 
             let tdDescElement = document.createElement("td");
             tdDescElement.innerHTML = item.description.substring(0, 20) + "...";
@@ -26,6 +30,8 @@ function onButtonClick() {
             audio.appendChild(source);
             tdTeaserElement.appendChild(audio);
             row.appendChild(tdTeaserElement);
+            let br = document.createElement("br");
+            row.appendChild(br);
             table.appendChild(row);
         });
     });
